765e841d1148b89519509ffd87cada777f712bcc
Luis Fernando de Souza Moro <luis.moro@csgif.com.br>
2016-11-29 08:50:59 -0200

    Commit Inicial
    Inserção de documentos no MongoDB
    Criação de endPoints para findAll e findById
    Criação de Server para subir a API [Ainda não está funcionando]

---
+++ src/main/java/com/br/itau/internet/ReactiveEndPoints.java
@@ -0,0 +1,145 @@
+package com.br.itau.internet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.http.codec.BodyInserters;
+import org.springframework.http.server.reactive.HttpHandler;
+import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.ipc.netty.http.server.HttpServer;
+
+import java.util.Random;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class ReactiveEndPoints {
+
+	@Bean
+	RouterFunction<?> router(PersonHandler handler) {
+		return RouterFunctions.route(RequestPredicates.GET("/persons"), handler::all)
+				.and(RouterFunctions.route(RequestPredicates.GET("/persons/{id}"), handler::byId));
+	}
+
+	@Bean
+	HttpServer server (RouterFunction<?> router) {
+		HttpHandler handler = RouterFunctions.toHttpHandler(router);
+		HttpServer httpServer = HttpServer.create(8080);
+		httpServer.newHandler(new ReactorHttpHandlerAdapter(handler));
+		return httpServer;
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveEndPoints.class, args);
+	}
+}
+
+@Component
+class PersonHandler {
+
+	private final PersonRepository personRepository;
+
+	@Autowired
+	PersonHandler(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
+
+	ServerResponse<Flux<Person>> all(ServerRequest request) {
+		Flux<Person> flux = Flux.fromStream(personRepository.all());
+		return ServerResponse.ok().body(BodyInserters.fromPublisher(flux, Person.class));
+	}
+
+	ServerResponse<Mono<Person>> byId(ServerRequest request) {
+		String id = request.pathVariable("id");
+		return ServerResponse.ok().body(BodyInserters.fromPublisher(Mono.fromFuture(personRepository.findById(id)), Person.class));
+	}
+}
+
+@Component
+class SampleDataCLR implements CommandLineRunner {
+
+	private final PersonRepository personRepository;
+
+	@Autowired
+	public SampleDataCLR(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		Stream.of("Luis Moro", "Renan Barbioni", "André Formento", "Antonio Neto", "Emerson Muraro")
+		.forEach( name -> personRepository.save(new Person(name, new Random().nextInt(100))));
+
+		personRepository.findAll().forEach(System.out::println);
+	}
+}
+
+interface PersonRepository extends MongoRepository<Person, String> {
+
+	CompletableFuture<Person> findById(String id);
+
+	@Query("{}")
+	Stream<Person> all();
+}
+
+@Document
+class Person {
+
+	@Id
+	private String id;
+
+	private String name;
+
+	private int age;
+
+	public Person() {
+	}
+
+	public Person(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Person{" +
+				"id='" + id + '\'' +
+				", name='" + name + '\'' +
+				", age=" + age +
+				'}';
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
---
+++ src/test/java/com/br/itau/internet/FakeApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.br.itau.internet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FakeApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
