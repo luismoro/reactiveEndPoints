5c3ec8b16b4e2a72b89a5f41b6b04356f4c6be19
Luis Fernando de Souza Moro <luis.moro@csgif.com.br>
2016-12-13 13:57:48 -0200

    Insere metodo que popula o banco

---
+++ src/main/java/com/br/itau/internet/SampleDataCLR.java
@@ -0,0 +1,27 @@
+package com.br.itau.internet;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by luismoro on 13/12/16.
+ */
+@Component
+public class SampleDataCLR implements CommandLineRunner {
+
+    private final PersonRepository personRepository;
+
+    @Autowired
+    public SampleDataCLR(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @Override
+    public void run(String... strings) throws Exception {
+//		Stream.of("Teste")
+//		.forEach( name -> personRepository.save(new Person(name, new Random().nextInt(100))));
+//
+//		personRepository.findAll().forEach(System.out::println);
+    }
+}
